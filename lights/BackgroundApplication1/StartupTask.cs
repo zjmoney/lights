af99d5845a909907f78d543a8421acf3f277a77b
Zach Nehrenberg <zachnehrenberg@hotmail.com>
2015-08-18 22:19:25 -0700

    xvdfgdfvg

--- lights/BackgroundApplication1/StartupTask.cs
+++ lights/BackgroundApplication1/StartupTask.cs
@@ -7,7 +7,7 @@ using Windows.ApplicationModel.Background;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
-namespace BackgroundApplication1
+namespace CoolLights
 {
     public sealed class StartupTask : IBackgroundTask
     {
